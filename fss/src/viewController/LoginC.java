7304ca9df069ab217349d02fd46a8f00b8036850
igneH <alexander1pixner@gmail.com>
2023-05-16 10:24:33 +0200

    Login

---
+++ fss/src/viewController/LoginC.java
@@ -0,0 +1,59 @@
+package viewController;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginC {
+    @FXML
+    private TextField tfUsername;
+    @FXML
+    private TextField tfPassword;
+    @FXML
+    private Button btLogin;
+    @FXML
+    void btLoginOnAction(ActionEvent event){
+        login();
+    }
+    public static void show(Stage stage) {
+        try {
+
+            FXMLLoader loader = new FXMLLoader(LoginC.class.getResource("Login.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Login FSS");
+            stage.show();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            Platform.exit();
+        }
+    }
+    public void initialize () {
+    }
+
+    private void login() {
+
+    }
+
+    private void info(String msg){
+        Alert info = new Alert(Alert.AlertType.INFORMATION, msg);
+        info.showAndWait();
+    }
+
+    private void error(String msg){
+        Alert err = new Alert(Alert.AlertType.ERROR, msg);
+        err.showAndWait();
+    }
+}
+
